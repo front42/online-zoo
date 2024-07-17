10c5e39aea9363f69a6c54013dd356e78eec22c4
front42 <altaifront@gmail.com>
2024-07-17 13:39:33 +0300

    feat: add aside-menu

--- pages/zoos.js
+++ pages/zoos.js
@@ -1,9 +1,28 @@
 'use strict';
 
+asideBtn.addEventListener('click', () => {
+  burger.classList.remove('active');
+  nav.classList.remove('active');
+  asideBtn.style.opacity = 0;
+  aside.classList.add('aside-visible');
+});
+
+aside.querySelectorAll('img').forEach(link =>
+  link.addEventListener('click', () => {
+    aside.classList.remove('aside-visible');
+    asideBtn.style.opacity = 1;
+  })
+);
+
 const mainView = document.querySelector('.animal iframe');
 const previews = document.querySelectorAll('.animal__small iframe');
 const playBtns = document.querySelectorAll('.overlay__btn');
-playBtns.forEach(btn => btn.addEventListener('click', event => {
-  const preview = event.target.closest('.animal__small').querySelector('iframe');
-  [preview.src, mainView.src] = [mainView.src, preview.src];
-}));
+
+playBtns.forEach(btn =>
+  btn.addEventListener('click', event => {
+    const preview = event.target
+      .closest('.animal__small')
+      .querySelector('iframe');
+    [preview.src, mainView.src] = [mainView.src, preview.src];
+  })
+);
--- script.js
+++ script.js
@@ -1,18 +1,32 @@
+'use strict';
+
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav');
 const navLinks = document.querySelectorAll('.nav__link');
 const slogan = document.querySelector('.intro__slogan');
+const aside = document.querySelector('.aside');
+const asideBtn = document.querySelector('.aside__btn');
+
+document.addEventListener('scroll', () => {
+  burger.classList.remove('active');
+  nav.classList.remove('active');
+  slogan ? slogan.classList.remove('invisible') : null;
+  aside ? aside.classList.remove('aside-visible') : null;
+  asideBtn ? asideBtn.style.opacity = 1 : null;
+});
 
 burger.addEventListener('click', () => {
   burger.classList.toggle('active');
   nav.classList.toggle('active');
-  slogan.classList.toggle('invisible');
+  slogan ? slogan.classList.toggle('invisible') : null;
+  aside ? aside.classList.remove('aside-visible') : null;
+  asideBtn ? asideBtn.style.opacity = 1 : null;
 });
 
 navLinks.forEach(link =>
   link.addEventListener('click', () => {
     burger.classList.remove('active');
     nav.classList.remove('active');
-    slogan.classList.remove('invisible');
+    slogan ? slogan.classList.remove('invisible') : null;
   })
 );
